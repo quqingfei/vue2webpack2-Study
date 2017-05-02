8f4113a27bc32c289bda2a3324afa9bd8af66265
quqingfei <474769606@qq.com>
2017-05-02 18:14:45 +0800

    add vue-router2.0 and tab-swiper

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <div id="app">
+  <div>
     <v-header></v-header>
-    <div class="tab">
-      <div class="tab-item">商品</div>
-      <div class="tab-item">评价</div>
-      <div class="tab-item">商家</div>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评价</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
     </div>
-    <div class="footer">I`m footer</div>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -20,13 +26,23 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
-#app
-  .tab
-    display: flex
-    width: 100%
-    height: 40px
-    line-height: 40px
-    .tab-item
-      flex: 1
-      text-align: center
+  @import "./common/stylus/mixin.styl"
+.tab
+  display flex
+  width 100%
+  height 40px
+  line-height 40px
+  border-1px(rgba(7,17,27,0.1))
+  .tab-item
+    flex 1
+    text-align center
+    & > a
+      display block
+      width 100%
+      height 100%
+      text-decoration none
+      font-size 14px
+      color rgb(77,85,93)
+      &.active
+        color rgb(240,20,20)
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,33 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App';
+import goods from '@/components/goods/goods';
+import ratings from '@/components/ratings/ratings';
+import seller from '@/components/seller/seller';
 
 Vue.config.productionTip = false;
 
+Vue.use(VueRouter);
+
+let app = Vue.extend(App);
+
+const routes = [
+  { path: '/goods', component: goods },
+  { path: '/ratings', component: ratings },
+  { path: '/seller', component: seller }
+];
+
+const router = new VueRouter({
+  routes,
+  linkActiveClass: 'active'
+});
+
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-});
+  router,
+  render: h => h(app)
+}).$mount('#app');
+
+router.push('./goods');
