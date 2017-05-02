890297f2f48ba1c02d3aad8717e5609fddd7b728
quqingfei <474769606@qq.com>
2017-05-02 10:24:06 +0800

    add some stylus and modules

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评价</div>
+      <div class="tab-item">商家</div>
+    </div>
+    <div class="footer">I`m footer</div>
+  </div>
+</template>
+
+<script>
+  import vHeader from './components/header/header';
+  export default {
+    components: {
+      vHeader
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+#app
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    .tab-item
+      flex: 1
+      text-align: center
+</style>
