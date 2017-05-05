b7fd2416888bb089e6d3d6d0fe88c23f37bcdf3a
quqingfei <474769606@qq.com>
2017-05-05 18:14:05 +0800

    half complete header

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -11,12 +11,19 @@
         </div>
         <div class="description">{{seller.description}}/{{seller.deliveryTime}}分钟送达</div>
         <div v-if="seller.supports" class="supports">
-          <span class="icon"></span>
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
+      <div v-if="seller.supports" class="support-count">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
     </div>
-    <div class="bulletin-wrapper"></div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right iconright"></i>
+    </div>
 	</div>
 </template>
 
@@ -26,6 +33,9 @@
       seller: {
         type: Object
       }
+    },
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
     }
 	};
 </script>
@@ -34,17 +44,19 @@
 @import "../../common/stylus/mixin.styl" 
 
   .header
-    background-color #000
+    background-color #666
     color #fff
     .content-wrapper
+      position relative
       padding 24px 12px 18px 24px
       font-size 0
       .avatar
         vertical-align top
         display inline-block
+        img
+          border-radius 2px
       .content
         display inline-block
-        font-size 14px
         margin-left 16px
         .title
           margin 2px 0 8px 0
@@ -60,5 +72,74 @@
             margin-left 6px
             font-size 16px
             line-height 18px
-            font-weight bold  
+            font-weight bold
+        .description
+          margin-bottom 10px
+          line-height 12px
+          font-size 12px
+        .supports
+          height value
+          .text
+            font-size 10px
+            line-height 12px
+          .icon
+            vertical-align top
+            display inline-block
+            width 12px
+            height 12px
+            margin-right 4px
+            background-size 12px 12px
+            background-repeat no-repeat
+            &.decrease
+              bg-image('decrease_1')
+            &.discount
+              bg-image('discount_1')
+            &.guarantee
+              bg-image('guarantee_1')
+            &.invoice
+              bg-image('invoice_1')
+            &.special
+              bg-image('special_1')    
+      .support-count
+        position absolute
+        right 12px
+        bottom 14px
+        padding 0 8px
+        height 24px
+        line-height 24px
+        border-radius 14px
+        background-color rgba(0,0,0,0.2)
+        .count
+          vertical-align top
+          font-size 10px
+        .icon-keyboard_arrow_right
+          margin-left 2px
+          line-height 24px
+          font-size 10px
+    .bulletin-wrapper
+      height 28px
+      line-height 28px
+      padding 0 22px 0 12px
+      white-space nowrap
+      overflow hidden
+      text-overflow ellipsis
+      position relative
+      .bulletin-title
+        vertical-align top
+        margin-top 7px
+        display inline-block
+        width 22px
+        height 12px
+        bg-image('bulletin')
+        background-size 22px 12px
+        background-repeat no-repeat
+      .bulletin-text
+         font-size 10px
+         margin 0 4px
+         vertical-align top
+    .iconright
+      position absolute   
+      font-size 10px
+      right 8px
+      top 8px
 </style>
